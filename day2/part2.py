4926bfd890f6722b475852eb7dfcc99462703daf
Mwest2020 <mwesterweel@hotmail.com>
2025-08-19 21:30:29 +0200

    day2

--- day2/part2.py
+++ day2/part2.py
@@ -3,29 +3,25 @@ filename = "./reports.txt"
 
 def is_safe(nums, limit=3):
     def inc(seq):
-        return all(seq[i] < seq[i + 1] for i in range(len(seq) - 1))
+        return all(0 < seq[i + 1] - seq[i] <= limit for i in range(len(seq) - 1))
 
     def dec(seq):
-        return all(seq[i] > seq[i + 1] for i in range(len(seq) - 1))
+        return all(0 < seq[i] - seq[i + 1] <= limit for i in range(len(seq) - 1))
 
     def passes(seq):
-        threshold = all(abs(seq[i + 1] - seq[i]) <= limit for i in range(len(seq) - 1))
-        safe = inc(seq) or dec(seq)
-        return threshold and safe
+        return inc(seq) or dec(seq)
 
     if passes(nums):
         return True
 
     for k in range(len(nums)):
-        reduced = nums[:k] + nums[k + 1 :]
-        if passes(reduced):
+        if passes(nums[:k] + nums[k + 1 :]):
             return True
     return False
 
 
 safe = 0
 unsafe = 0
-
 with open(filename, "r") as file:
     for line in file:
         nums = list(map(int, line.split()))
@@ -34,5 +30,5 @@ with open(filename, "r") as file:
         else:
             unsafe += 1
 
-print(unsafe)
-print(safe)
+print("unsafe:", unsafe)
+print("safe:", safe)
